dac3827ef184c0c0c1c873d453efd4d90c11e9d8
Nikhil Singh | Contributor <nikhillsinghh1020@gmail.com>
2025-11-03 00:35:28 +0530

    refactor navbar pages

--- js/Nav.js
+++
@@ -1,7 +0,0 @@
-  // Load navigation component
-    fetch('navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-      })
-      .catch(error => console.error('Error loading navigation:', error));
