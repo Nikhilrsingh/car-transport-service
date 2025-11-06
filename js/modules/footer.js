4752367d4c1696a5e78f2ebd365bacf31a04ce48
Nikhil Singh | Contributor <141393080+Nikhilrsingh@users.noreply.github.com>
2025-11-06 16:59:26 +0530

    Revert "feat: Add automatic copyright year update in footer"

--- js/modules/footer.js
+++ js/modules/footer.js
@@ -1,15 +1,3 @@
 // Back to Top Functionality - Moved to js/modules/back-to-top-button.js
 // This module previously handled back-to-top button logic but is now centralized
 // in the dedicated back-to-top-button.js module to avoid duplication
-// js/modules/footer.js
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch('./pages/footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer-container').innerHTML = data;
-      const yearSpan = document.getElementById('current-year');
-      if (yearSpan) yearSpan.textContent = new Date().getFullYear();
-    })
-    .catch(err => console.error(err));
-});
